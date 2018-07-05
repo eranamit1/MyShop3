e6f03af669dd18fe0f012afc70cbcc39f48a628e
eran amit <eran@amitda.com>
2018-07-05 19:02:26 +0300

    Proudct Manager pages built and tested

---
+++ MyShop/MyShop.Core/Models/Proudct.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.Core.Models
+{
+   public  class Proudct
+    {
+        public string Id { get; set; }
+        [StringLength(20)]
+        [DisplayName("Proudct Name")]
+        public string Name { get; set; }
+        public string Descrition { get; set; }
+        [Range(0,1000)]
+        public decimal Price { get; set; }
+        public string Catgorey { get; set; }
+        public string Image { get; set; }
+        public Proudct()
+        {
+            this.Id = Guid.NewGuid().ToString();
+        }
+    }
+}
---
+++ MyShop/MyShop.WebUI/Controllers/ProudctMnagerController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyShop.Core.Models;
+using MyShop.DataAccess.InMemorey;
+
+namespace MyShop.WebUI.Controllers
+{
+    public class ProudctMnagerController : Controller
+    {
+        ProudctRepostorey context;
+        public ProudctMnagerController()
+        {
+            context = new ProudctRepostorey();
+        }
+        // GET: ProudctMnager
+        public ActionResult Index()
+        {
+            List<Proudct> proudcts = context.Collection().ToList();
+            return View(proudcts);
+        }
+        public ActionResult Create() { 
+            Proudct proudct = new Proudct();
+            return View(proudct);
+        }
+        [HttpPost]
+        public ActionResult Create(Proudct proudct)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(proudct);
+            }
+            else
+            {
+                context.Insert(proudct);
+                context.Commit();
+                return RedirectToAction("Index");
+            }
+        }
+        public ActionResult Edit(string Id)
+        {
+            Proudct proudct = context.Find(Id);
+            if (proudct == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(proudct);
+            }
+        }
+        [HttpPost]
+        public ActionResult Edit(Proudct proudct,string Id)
+        {
+            Proudct proudctToEdit = context.Find(Id);
+            if (proudctToEdit == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                if (!ModelState.IsValid)
+                {
+                    return View(proudct);
+                }
+                proudctToEdit.Catgorey = proudct.Catgorey;
+                proudctToEdit.Descrition = proudct.Descrition;
+                proudctToEdit.Image = proudct.Image;
+                proudctToEdit.Name = proudct.Name;
+                proudctToEdit.Price = proudct.Price;
+                context.Commit();
+                return RedirectToAction("Index");
+            }
+            
+        }
+        public ActionResult Delete(string Id)
+        {
+            Proudct proudctToDelete = context.Find(Id);
+            if (proudctToDelete == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(proudctToDelete);
+            }
+        }
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult ConfirmDelete(string Id)
+        {
+            Proudct proudctToDelete = context.Find(Id);
+            if (proudctToDelete == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                context.Delete(Id);
+                context.Commit();
+                return RedirectToAction("Index");
+            }
+        }
+    }
+}
